79c4501b69db63b5778440b47e11eb7db3e49141
Tayfun Cesur <tayfuncesur35@gmail.com>
2023-05-08 16:39:12 +0200

    tayfun-optimization-2

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,23 +4,13 @@ pragma solidity 0.8.19;
 contract GasContract {
 
     mapping(address => uint256) public balances;
-    //mapping(address => uint256) internal whiteListStruct;
-    mapping(address => ImportantStruct) internal whiteListStruct;
+    mapping(address => uint256) internal whiteListStruct;
     mapping(address => uint256) public whitelist;
 
     address private immutable contractOwner;
     address[5] public administrators;
-    
-    struct ImportantStruct {
-        uint256 amount;
-        bool paymentStatus;
-        address sender;
-    }
-
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
-
-    error InsufficientBalance();
     constructor(address[] memory _admins, uint256 _totalSupply) {
         contractOwner = msg.sender;
         assembly{
@@ -31,6 +21,7 @@ contract GasContract {
             sstore(add(administrators.slot,3), mload(add(startPos, 0x60)))
             sstore(add(administrators.slot,4), mload(add(startPos, 0x80)))
         }
+
 
         if(_admins[0] == msg.sender){
             balances[_admins[0]] = _totalSupply;
@@ -46,49 +37,31 @@ contract GasContract {
         
     }
 
-    function balanceOf(address _user) public view returns (uint256 balance_) {
-        uint256 balance = balances[_user];
-        return balance;
+    function balanceOf(address _user) public view returns (uint256) {
+        return balances[_user];
     }
 
     function transfer(address _recipient, uint256 _amount, string calldata _name) public {
-       
-        if(balances[msg.sender] < _amount){
-            revert InsufficientBalance();
-        }
-        
-        require(bytes(_name).length < 9, "Name exceeds");
-        
+        require(bytes(_name).length < 9);
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
-  
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
-        
         require(msg.sender == contractOwner);
         require(_tier < 255);
-        
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
     function whiteTransfer(address _recipient, uint256 _amount) public {
-
-        if(balances[msg.sender] < _amount || _amount < 3){
-            revert InsufficientBalance();
-        }
-
-        whiteListStruct[msg.sender] = ImportantStruct(_amount, true, msg.sender);
-       
+        whiteListStruct[msg.sender] = _amount;
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
-        balances[msg.sender] += whitelist[msg.sender];
-        balances[_recipient] -= whitelist[msg.sender];
         emit WhiteListTransfer(_recipient);
     }
 
     function getPaymentStatus(address sender) public view returns (bool, uint256) {        
-        return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
+        return (true, whiteListStruct[sender]);
     }
 
 }
